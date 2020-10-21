0b660d47ad471fb01e2a3d6f7e309cd39e37a66d
Zeeshan Ahmad <shanidoger@gmail.com>
2020-10-21 10:19:32 +0300

    Update PayFunc.py

--- PayFunc.py
+++ PayFunc.py
@@ -1,8 +1,11 @@
 
-#declare function called by monthly pay
-def Monthlypay(hours, rate): #function have 2 arguments, So user will inform by monthly hours and rates
-    Pay = hours * rate
-    return Pay
+    #declare function called by monthly pay
+    #function have 2 arguments,
+    # So user will inform by monthly hours and rates
+def Monthlypay(hours, rate):
+        Pay = hours * rate
+        return Pay
+    
 
-#call fucntion and print 
+    #call fucntion and print 
 print(Monthlypay(160,12))
